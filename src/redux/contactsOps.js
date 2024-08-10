bbb0339a43dc7e806a95889c626ddf4c9e2aae1c
AndriyV2504 <andreyvolyanik@gmail.com>
2024-08-10 12:17:40 +0300

    createAsyncThunk

--- src/redux/contactsOps.js
+++ src/redux/contactsOps.js
@@ -1,12 +1,28 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 axios.defaults.baseURL = "https://66b7087f7f7b1c6d8f1ab6e0.mockapi.io/";
 
-export const fetchContacts = () => async () => {
-  try {
-    const response = await axios.get("/contacts");
-    return response.data;
-  } catch (error) {
-    console.log(error);
+export const fetchContacts = createAsyncThunk(
+  "contacts/fetchAll",
+  async (_, thunkAPI) => {
+    try {
+      const response = await axios.get("/contacts");
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
   }
-};
+);
+
+export const deleteContact = createAsyncThunk(
+  "contacts/deleteContact",
+  async (id, thunkAPI) => {
+    try {
+      await axios.delete(`/contacts/${id}`);
+      return id;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
