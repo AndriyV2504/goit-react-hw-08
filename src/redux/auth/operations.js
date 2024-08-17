002468b5eccd6c62d0f248e0de156d485b841437
AndriyV2504 <andreyvolyanik@gmail.com>
2024-08-17 16:32:51 +0300

    baseURL correct

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -1,13 +1,11 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
-
-axios.defaults.baseURL = "https://connections-api.goit.global/";
+import { goItApi } from "../../config/goItApi";
 
 export const register = createAsyncThunk(
   "auth/register",
   async (user, thunkAPI) => {
     try {
-      const response = await axios.post("/users/signup", user);
+      const response = await goItApi.post("/users/signup", user);
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
