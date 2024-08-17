87fddb1895b430bc5e4df69e6969aeba07f3aef9
AndriyV2504 <andreyvolyanik@gmail.com>
2024-08-17 14:12:35 +0300

    UserMenu

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,11 +1,13 @@
 import AuthNav from "../AuthNav/AuthNav";
 import Navigation from "../Navigation/Navigation";
+import UserMenu from "../UserMenu/UserMenu";
 
 const AppBar = () => {
   return (
     <div>
       <Navigation />
       <AuthNav />
+      <UserMenu />
     </div>
   );
 };
