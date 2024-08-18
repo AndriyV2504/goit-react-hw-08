fa333eed8dca12008679a88e3d991b134a441c92
AndriyV2504 <andreyvolyanik@gmail.com>
2024-08-18 11:46:47 +0300

    PersistGate

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,12 @@
+import styles from "./HomePage.module.css";
 const HomePage = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Welcome to Our Website!</h1>
+      <p className={styles.text}>
+        We're glad to have you here. Explore our site and discover everything we
+        have to offer.
+      </p>
     </div>
   );
 };
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,37 @@ import { configureStore } from "@reduxjs/toolkit";
 import contactsSlice from "./contacts/slice";
 import filtersSlice from "./filters/slice";
 import { authReducer } from "./auth/slice";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+  whitelist: ["token"],
+};
 
 export const store = configureStore({
   reducer: {
     contacts: contactsSlice,
     filter: filtersSlice,
-    auth: authReducer,
+    auth: persistReducer(persistConfig, authReducer),
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
