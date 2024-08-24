04b85b7f415ead7e11ef2e4716c321c9ccdf4d9e
AndriyV2504 <andreyvolyanik@gmail.com>
2024-08-24 13:58:10 +0300

    isLoggedIn AuthNav

--- src/components/AuthNav/AuthNav.jsx
+++ src/components/AuthNav/AuthNav.jsx
@@ -2,29 +2,36 @@ import { NavLink } from "react-router-dom";
 import styles from "./AuthNav.module.css";
 import clsx from "clsx";
 import UserMenu from "../UserMenu/UserMenu";
+import { useSelector } from "react-redux";
+import { selectIsLoggedIn } from "../../redux/auth/selectors";
 
 const buildLinkClass = ({ isActive }) => {
   return clsx(styles.link, isActive && styles.active);
 };
 
 const AuthNav = () => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+
   return (
     <header className={styles.header}>
-      <UserMenu />
-      <nav>
-        <ul className={styles.nav}>
-          <li>
-            <NavLink className={buildLinkClass} to="/register">
-              Register
-            </NavLink>
-          </li>
-          <li>
-            <NavLink className={buildLinkClass} to="/login">
-              Login
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
+      {isLoggedIn ? (
+        <UserMenu />
+      ) : (
+        <nav>
+          <ul className={styles.nav}>
+            <li>
+              <NavLink className={buildLinkClass} to="/register">
+                Register
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className={buildLinkClass} to="/login">
+                Login
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      )}
     </header>
   );
 };
